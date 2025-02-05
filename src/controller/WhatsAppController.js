0dbfe3588c0d80008fc80612b355c7421d9cfcd9
Silvio Seferin <silvioseferin@gmail.com>
2025-02-05 18:11:12 -0300

    Adicionando eventos para abrir e fechar o painel de editar perfil e adicionar contato

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -2,10 +2,11 @@ class WhatsAppController {
 
     constructor() {
 
-        console.log('Ok');
+        console.log('Controller ok');
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
     }
 
     loadElements() {
@@ -84,6 +85,48 @@ class WhatsAppController {
 
             return this.classList.contains(name);
         }
+    }
+
+    initEvents() {
+
+        this.el.myPhoto.on('click', e => {
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+
+
+        });
+        this.el.btnNewContact.on('click', e => {
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+
+            this.el.panelEditProfile.removeClass('open');
+
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+
+            this.el.panelAddContact.removeClass('open');
+
+        });
+    }
+
+    closeAllLeftPanel() {
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
+
     }
 
 }
