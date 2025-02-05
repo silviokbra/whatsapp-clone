3c5a203d49c7fcc369ae7fe71c69a6a044bc9582
Silvio Seferin <silvioseferin@gmail.com>
2025-02-05 17:44:43 -0300

    Adicionando prototype

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
 
         console.log('Ok');
 
+        this.elementsPrototype();
         this.loadElements();
     }
 
@@ -16,6 +17,73 @@ class WhatsAppController {
             this.el[Format.getCamelCase(element.id)] = element;
 
         });
+
+    }
+
+    elementsPrototype() {
+
+        Element.prototype.hide = function () {
+
+            this.style.display = 'none';
+            return this;
+
+        }
+        Element.prototype.show = function () {
+
+            this.style.display = 'block';
+            return this;
+
+        }
+
+        Element.prototype.toggle = function () {
+
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+            return this;
+        }
+
+        Element.prototype.on = function (events, fn) {
+
+            events.split(' ').forEach(event => {
+
+                this.addEventListener(event, fn);
+
+            });
+            return this;
+
+        }
+
+        Element.prototype.css = function (styles) {
+
+            for (let name in styles) {
+                this.style[name] = styles[name];
+
+            }
+            return this;
+
+        }
+
+        Element.prototype.addClass = function (name) {
+
+            this.classList.add(name);
+            return this;
+        }
+
+        Element.prototype.removeClass = function (name) {
+
+            this.classList.remove(name);
+            return this;
+        }
+
+        Element.prototype.toggleClass = function (name) {
+
+            this.classList.toggle(name);
+            return this;
+        }
+
+        Element.prototype.hasClass = function (name) {
+
+            return this.classList.contains(name);
+        }
     }
 
 }
