6f4325ca39762fcb75e7eb702b9b16325c264407
Silvio Seferin <silvioseferin@gmail.com>
2025-02-05 22:03:31 -0300

    Adicionando eventos do campo digitar mensagem

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -285,6 +285,52 @@ class WhatsAppController {
 
         });
 
+
+        this.el.inputText.on('keypress', e => {
+
+            if (e.key === 'Enter' && !e.ctrlKey) {
+
+                e.preventDefault();
+                this.el.btnSend.click();
+            }
+
+        });
+
+        this.el.inputText.on('keyup', e => {
+            const text = this.el.inputText.textContent.trim();
+
+            if (text.length > 0) {
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+            } else {
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+            }
+        });
+        this.el.btnSend.on('click', e => {
+
+            console.log(this.el.inputText.innerHTML);
+
+        });
+
+
+        this.el.btnEmojis.on('click', e => {
+
+            this.el.panelEmojis.toggleClass('open');
+
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+
+            emoji.on('click', e => {
+
+                console.log(emoji);
+            })
+
+        })
+
     }
 
     startRecordMicrophoneTime() {
