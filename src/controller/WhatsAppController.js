c44819fcdabe0d93e5d4bf73d354a715562f248b
Silvio Seferin <silvioseferin@gmail.com>
2025-02-09 01:09:19 -0300

    Enviando mensagem para o firebase

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -5,6 +5,7 @@ import { DocumentPreviewController } from './DocumentPreviewController';
 import { Firebase } from './../util/Firebase';
 import { User } from './../model/User';
 import { Chat } from './../model/Chat'
+import { Message } from './../model/Message'
 
 export class WhatsAppController {
 
@@ -145,23 +146,7 @@ export class WhatsAppController {
 
                 div.on('click', e => {
 
-                    console.log(contact.chatiD);
-
-                    this.el.activeName.innerHTML = contact.name;
-                    this.el.activeStatus.innerHTML = contact.status;
-
-                    if (contact.photo) {
-
-                        let img = this.el.activePhoto;
-                        img.src = contact.photo;
-                        img.show();
-
-                    }
-
-                    this.el.home.hide();
-                    this.el.main.css({
-                        display: 'flex'
-                    });
+                    this.setActiveChat(contact);
 
                 });
 
@@ -172,6 +157,29 @@ export class WhatsAppController {
         });
 
         this._user.getContacts()
+    }
+
+
+    setActiveChat(contact) {
+
+        this._contactActive = contact;
+
+        this.el.activeName.innerHTML = contact.name;
+        this.el.activeStatus.innerHTML = contact.status;
+
+        if (contact.photo) {
+
+            let img = this.el.activePhoto;
+            img.src = contact.photo;
+            img.show();
+
+        }
+
+        this.el.home.hide();
+        this.el.main.css({
+            display: 'flex'
+        });
+
     }
 
 
@@ -620,7 +628,15 @@ export class WhatsAppController {
         });
         this.el.btnSend.on('click', e => {
 
-            console.log(this.el.inputText.innerHTML);
+            Message.send(
+                this._contactActive.chatId,
+                this._user.email,
+                'text',
+                this.el.inputText.innerHTML
+            )
+
+            this.el.inputText.innerHTML = '';
+            this.el.panelEmojis.removeClass('open');
 
         });
 
