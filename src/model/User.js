c5e833b60452fbb4cffc5aeb996fdd36153bb095
Silvio Seferin <silvioseferin@gmail.com>
2025-02-08 23:30:04 -0300

    Listando contato na tela

--- src/model/User.js
+++ src/model/User.js
@@ -45,6 +45,13 @@ export class User extends Model {
 
     }
 
+    static getContactsRef(id) {
+
+        return User.getRef()
+            .doc(id)
+            .collection('contacts');
+    }
+
     static findByEmail(email) {
 
         return User.getRef().doc(email);
@@ -52,12 +59,36 @@ export class User extends Model {
 
     addContact(contact) {
 
-        return User.getRef()
-            .doc(this.email)
-            .collection('contacts')
+        return User.getContactsRef(this.email)
             .doc(btoa(contact.email))
             .set(contact.toJSON());
 
+    }
+
+    getContacts() {
+
+        return new Promise((s, f) => {
+
+            User.getContactsRef(this.email).onSnapshot(docs => {
+
+                let contacts = [];
+
+                docs.forEach(doc => {
+
+                    let data = doc.data();
+
+                    data.id = doc.id;
+
+                    contacts.push(data);
+
+                });
+
+                this.trigger('contactschange', docs);
+
+                s(contacts);
+            })
+
+        })
     }
 
 }
