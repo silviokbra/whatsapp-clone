052d9d4cc57be8859a3217ad690ad1e40b102649
Silvio Seferin <silvioseferin@gmail.com>
2025-02-09 02:49:38 -0300

    Scroll descer automatico

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -186,16 +186,24 @@ export class WhatsAppController {
             display: 'flex'
         });
 
+
+
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp').onSnapshot(docs => {
 
             this.el.panelMessagesContainer.innerHTML = '';
+
+            let scrollTop = this.el.panelMessagesContainer.scrollTop;
+            let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+            let autoScroll = (scrollTop >= scrollTopMax);
 
             docs.forEach(doc => {
 
                 let data = doc.data();
                 data.id = doc.id;
 
-                if (!this.el.panelMessagesContainer.querySelector('#' + data.id)) {
+                if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)) {
+
+
                     let message = new Message();
 
                     message.fromJSON(data);
@@ -208,7 +216,15 @@ export class WhatsAppController {
 
                 }
 
-            })
+            });
+            if (autoScroll) {
+
+                this.el.panelMessagesContainer.scrollTop = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+
+            } else {
+
+                this.el.panelMessagesContainer.scrollTop = scrollTop;
+            }
 
         });
 
