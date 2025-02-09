09d78982e9dbb2c3a71641ed94ec5d29ad1fb68a
Silvio Seferin <silvioseferin@gmail.com>
2025-02-09 00:53:44 -0300

    Criando class chat

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ import { MicrophoneController } from './MicrophoneController';
 import { DocumentPreviewController } from './DocumentPreviewController';
 import { Firebase } from './../util/Firebase';
 import { User } from './../model/User';
+import { Chat } from './../model/Chat'
 
 export class WhatsAppController {
 
@@ -143,6 +144,8 @@ export class WhatsAppController {
 
 
                 div.on('click', e => {
+
+                    console.log(contact.chatiD);
 
                     this.el.activeName.innerHTML = contact.name;
                     this.el.activeStatus.innerHTML = contact.status;
@@ -345,10 +348,20 @@ export class WhatsAppController {
 
                 if (data.name) {
 
-                    this._user.addContact(contact).then(() => {
+                    Chat.createIfNotExists(this._user.email, contact.email).then(chat => {
 
-                        this.el.btnClosePanelAddContact.click();
-                        console.info('Contato foi adicionado!');
+                        contact.chatId = chat.id;
+
+                        this._user.chatId = chat.id;
+
+                        contact.addContact(this._user);
+
+                        this._user.addContact(contact).then(() => {
+
+                            this.el.btnClosePanelAddContact.click();
+                            console.info('Contato foi adicionado!');
+
+                        });
 
                     });
 
