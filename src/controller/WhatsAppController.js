d261459c959d79657b90bfef04c7042fbc9e3105
Silvio Seferin <silvioseferin@gmail.com>
2025-02-08 21:39:01 -0300

    Salvando usuário autenticado no banco de dados

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -3,6 +3,7 @@ import { CameraController } from './CameraController';
 import { MicrophoneController } from './MicrophoneController';
 import { DocumentPreviewController } from './DocumentPreviewController';
 import { Firebase } from './../util/Firebase';
+import { User } from './../model/User';
 
 export class WhatsAppController {
 
@@ -20,10 +21,24 @@ export class WhatsAppController {
 
         this._firebase.initAuth()
             .then(response => {
-                this._user = response.user;
 
-                this.el.appContent.css({
-                    display: 'flex'
+                this._user = new User();
+
+                let userRef = User.findByEmail(response.user.email);
+
+                userRef.set({
+
+                    name: response.user.displayName,
+                    email: response.user.email,
+                    photo: response.user.photoURL
+
+                }).then(() => {
+
+                    this.el.appContent.css({
+                        display: 'flex'
+
+                    });
+
                 });
             }).catch(err => {
                 console.error(err);
