8a5694b656565c80d6d099d038551d1c48e6567c
Silvio Seferin <silvioseferin@gmail.com>
2025-02-08 22:45:52 -0300

    Atualizando dados de perfil do usuario no banco tambem

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -212,7 +212,16 @@ export class WhatsAppController {
 
         this.el.btnSavePanelEditProfile.on('click', e => {
 
-            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+            this.el.btnSavePanelEditProfile.disabled = true;
+
+            this._user.name = this.el.inputNamePanelEditProfile.innerHTML
+
+            this._user.save().then(() => {
+
+                this.el.btnSavePanelEditProfile.disabled = false;
+
+
+            });
 
         });
 
