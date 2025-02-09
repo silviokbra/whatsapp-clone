9e9590345d4650523251ff15f53b977922eba85f
Silvio Seferin <silvioseferin@gmail.com>
2025-02-08 21:16:38 -0300

    Adicionando firebase no projeto

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -2,17 +2,35 @@ import { Format } from './../util/Format';
 import { CameraController } from './CameraController';
 import { MicrophoneController } from './MicrophoneController';
 import { DocumentPreviewController } from './DocumentPreviewController';
+import { Firebase } from './../util/Firebase';
 
 export class WhatsAppController {
 
     constructor() {
-
-        console.log('Controller ok');
-
+        this._firebase = new Firebase();
+        this.initAuth();
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
+
+
     }
+
+    initAuth() {
+
+        this._firebase.initAuth()
+            .then(response => {
+                this._user = response.user;
+
+                this.el.appContent.css({
+                    display: 'flex'
+                });
+            }).catch(err => {
+                console.error(err);
+            })
+
+    }
+
 
     loadElements() {
 
---
+++ src/util/Firebase.js
@@ -0,0 +1,74 @@
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
+export class Firebase {
+    constructor() {
+
+
+        this._config = {
+            apiKey: "AIzaSyDq7omIfs5LYrtRWesQLJ7aO3gUmYxOW2k",
+            authDomain: "whatsapp-clone-808d8.firebaseapp.com",
+            projectId: "whatsapp-clone-808d8",
+            storageBucket: "whatsapp-clone-808d8.firebasestorage.app",
+            messagingSenderId: "254439604655",
+            appId: "1:254439604655:web:20ae2ada812d1845ff6f5a",
+            measurementId: "G-PHR5R86SVJ"
+        };
+        this.init();
+    }
+
+    init() {
+   
+        if (!window._initializedFirebase) {
+        
+            firebase.initializeApp(this._config);
+            firebase.firestore().settings({
+                timestampsInSnapshots: true
+            });
+            window._initializedFirebase = true;
+        }
+    }
+
+    static db() {
+        return firebase.firestore();
+    }
+
+    static hd() {
+        return firebase.storage();
+    }
+
+    initAuth() {
+
+        return new Promise((s, f) => {
+            let provider = new firebase.auth.GoogleAuthProvider();
+
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+
+                var token = result.credential.accessToken;
+
+                var user = result.user;
+
+                s({
+                    user,
+                    token
+                });
+
+            }).catch(function (error) {
+
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                var email = error.email;
+
+                var credential = error.credential;
+
+                f(error);
+
+            });
+
+        });
+
+    }
+
+}
